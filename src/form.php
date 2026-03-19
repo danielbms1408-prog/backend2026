be09bc4c509d4f3c934d3961f2ed8cb0d24b525e
danielbms1408-prog <danielbms1408@uni9.edu.br>
2026-03-19 00:29:16 +0000

    aula 18/03/26

---
+++ src/form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<html>
+    <body>
+        <h1>form - 18/03/26<h1>
+            <form method="post" action="recebe.php">
+login<input type="text" id="login" name="login"><br>
+senha<input type="password" id="senha" name="senha"><br>
+<input type="submit" value="validar">
+
+</form>
+</body>
+</html>
